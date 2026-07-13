8c91f7345b8b7916bc1a1c57b1bcd3f8d1359c60
Julian Grinblat <julian@dotcore.co.il>
2026-07-14 04:20:57 +0900

    chore: add prettier

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
-import { callbackify } from 'node:util';
+import { callbackify } from "node:util";
 
-import through from 'through2';
-import lua2js from 'redis-lua2js';
-import vinylToString from 'vinyl-contents-tostring';
-import PluginError from 'plugin-error';
+import through from "through2";
+import lua2js from "redis-lua2js";
+import vinylToString from "vinyl-contents-tostring";
+import PluginError from "plugin-error";
 
 // consts
-const PLUGIN_NAME = 'gulp-redis-lua2js';
+const PLUGIN_NAME = "gulp-redis-lua2js";
 
-const jsToVinyl = (file) => (js) => Object.assign(file, {
-  extname: '.js',
-  contents: file.isBuffer()
-    ? Buffer.from(js)
-    : file.isStream()
-      ? through().end(js)
-      : (() => { throw new PluginError(PLUGIN_NAME, 'Invalid file'); })(),
-});
+const jsToVinyl = (file) => (js) =>
+  Object.assign(file, {
+    extname: ".js",
+    contents: file.isBuffer()
+      ? Buffer.from(js)
+      : file.isStream()
+        ? through().end(js)
+        : (() => {
+            throw new PluginError(PLUGIN_NAME, "Invalid file");
+          })(),
+  });
 
 // plugin level function (dealing with files)
-export default ({ useFilenameAsName = true, name, ...options } = {}) => (
-  through.obj(callbackify((file, enc) => vinylToString(file, enc)
-    .then((lua) => lua2js(lua, {
-      ...options,
-      name: typeof name === 'string' && name.length > 0
-        ? name
-        : useFilenameAsName
-          ? file.stem
-          : undefined,
-    }))
-    .then(jsToVinyl(file))))
-);
+export default ({ useFilenameAsName = true, name, ...options } = {}) =>
+  through.obj(
+    callbackify((file, enc) =>
+      vinylToString(file, enc)
+        .then((lua) =>
+          lua2js(lua, {
+            ...options,
+            name:
+              typeof name === "string" && name.length > 0
+                ? name
+                : useFilenameAsName
+                  ? file.stem
+                  : undefined,
+          }),
+        )
+        .then(jsToVinyl(file)),
+    ),
+  );
